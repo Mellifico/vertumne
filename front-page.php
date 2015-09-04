0c5442eb6dc0af9004c5a9b4c3b4aa6ddabafdf9
Philippe Michou <mellifico@gmail.com>
2015-09-04 16:18:48 +0200

    various iterations...

--- front-page.php
+++ front-page.php
@@ -13,8 +13,7 @@
 <?php get_template_part('parts/top-bar'); ?>
 </div>
 <div class="wrapper">
-<div class="row">
-<div class="medium-6 large-6 columns">
+
 <div class="slider-a">
 <?php
 $args = array(
@@ -39,10 +38,7 @@ if ($attachments) {
 }
 ?>
 </div>
-</div>
-<div class="medium-6 large-6 columns"></div>
-</div>
-</div>
+
 <?php do_action('foundationPress_after_content'); ?>
 </div>
 </div>
